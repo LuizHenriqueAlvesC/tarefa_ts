cf011e2f40a37bfb63572cd69a9336f0d132d81e
luiz <luiz980alves@gmail.com>
2024-06-24 21:53:25 -0300

    initial commit

---
+++ src/tarefa.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(x, y) {
+    return x * y;
+}
+console.log(multiplicacao(10, 10));
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saudacao("Luiz"));
---
+++ src/tarefa.ts
@@ -0,0 +1,11 @@
+function multiplicacao(x: number, y: number): number {
+    return x * y;
+}
+
+console.log(multiplicacao(10, 10));
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(saudacao("Luiz"));
